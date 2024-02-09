ef6d04a08aa525124b3701c9a05582f5f478e343
jobygeorge99 <jobygeorge995@gmail.com>
2024-02-09 16:20:34 +0530

    security changes

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const userModel = require("../models/userModel")
+const { hash } = require("bcryptjs")
 const router = express.Router()
 
+hashPasswordGenerator = async(pass)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
 router.post("/signup",async(req,res)=>{
-    let data = req.body
-    let userModelObj=new userModel(data)
-    let result = await userModelObj.save()
-    res.send("yea")
+    //let data = req.body
+    let {data} = {"data":req.body}
+    let password = data.password
+
+    hashPasswordGenerator(password).then((hashedPassword)=>{
+        data.password=hashedPassword
+        console.log(hashedPassword)
+        console.log(data)
+
+        let userModelObj=new userModel(data)
+        let result = userModelObj.save()
+        res.json({
+            "status":"success"
+        })
+
+    })
+
 })
 
 module.exports=router
